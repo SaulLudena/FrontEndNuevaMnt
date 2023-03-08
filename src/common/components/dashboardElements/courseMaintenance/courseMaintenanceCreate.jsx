77304871343dbe1691fc1af26878c157c826c37c
SaulLudena <cornejo758@gmail.com>
2023-03-08 17:41:14 -0500

    updating project

--- src/common/components/dashboardElements/courseMaintenance/courseMaintenanceCreate.jsx
+++ src/common/components/dashboardElements/courseMaintenance/courseMaintenanceCreate.jsx
@@ -1,3 +1,4 @@
+import { Tab } from '@headlessui/react'
 import React from 'react'
 import TopNav from '../../topnav/topNav'
 export default function courseMaintenanceCreate() {
@@ -5,37 +6,24 @@ export default function courseMaintenanceCreate() {
     <div className="flex flex-1 w-full pt-5 pb-5 pr-5 overflow-hidden">
       <div className="bg-[#F7F7F7]  rounded-xl h-full p-9 flex flex-col gap-10 overflow-y-scroll scroller w-full ">
         <TopNav />
+        <div className="text-3xl font-bold  w-full grid gap-2">
+          <h1>Crea un nuevo curso</h1>
+        </div>
         <div className="">
-          <form className="grid 2xl:grid-cols-3 lg:grid-cols-2 grid-cols-1 gap-10">
-            <div>
-              <input
-                type="text"
-                placeholder="Nombre del curso"
-                className="py-4 px-5 w-full outline-none focus:border-2 focus:border-black border-2 border-white"
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                placeholder="Nombre del curso"
-                className="py-4 px-5 w-full outline-none focus:border-2 focus:border-black border-2 border-white"
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                placeholder="Nombre del curso"
-                className="py-4 px-5 w-full outline-none focus:border-2 focus:border-black border-2 border-white"
-              />
-            </div>
-            <div>
-              <input
-                type="text"
-                placeholder="Nombre del curso"
-                className="py-4 px-5 w-full outline-none focus:border-2 focus:border-black border-2 border-white"
-              />
-            </div>
-          </form>
+          <Tab.Group>
+            <Tab.List>
+              <Tab className="border-2 border-black p-5 rounded-tl-xl rounded-bl-xl">Informacion basica</Tab>
+              <Tab className="border-2 border-black p-5">Modulos</Tab>
+              <Tab className="border-2 border-black p-5">Lecciones</Tab>
+              <Tab className="border-2 border-black p-5 rounded-tr-xl rounded-br-xl">Recursos</Tab>
+            </Tab.List>
+            <Tab.Panels>
+              <Tab.Panel>fomulario de registro</Tab.Panel>
+              <Tab.Panel>formulario de modulos</Tab.Panel>
+              <Tab.Panel>formulario de lecciones</Tab.Panel>
+              <Tab.Panel>formulario de recursos</Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
         </div>
       </div>
     </div>
